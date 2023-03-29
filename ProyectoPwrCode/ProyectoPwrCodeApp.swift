f460ef60e3966e95ab56926e279d3b54ff2d6b4c
garciaraulcu <garciaraulcu@gmail.com>
2023-03-29 13:22:13 -0600

    Initial Commit

---
+++ ProyectoPwrCode/ProyectoPwrCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoPwrCodeApp.swift
+//  ProyectoPwrCode
+//
+//  Created by CEDAM04 on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoPwrCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
